72a526fea095b493205f472a8ad423806ad9bea0
Santanu <santattech@gmail.com>
2014-11-02 00:47:36 +0530

    maintanace page added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def site_under_comstruction
+    redirect_to "/maintanace"
+  end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
+  before_filter :site_under_comstruction, only: [:index]
+
   def index
+
   end
 end
