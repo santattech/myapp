77aef18a23534c7bb652c06b6204192e9354bd40
Santanu <santattech@gmail.com>
2014-12-03 00:29:33 +0530

    [#10], footer added

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,18 @@
 class HomesController < ApplicationController
-  before_filter :site_under_construction, only: []
+  before_filter :site_under_construction, only: [:about, :contact, :help]
 
   def index
     unless current_user
       redirect_to new_user_session_path
     end
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def help
   end
 end
