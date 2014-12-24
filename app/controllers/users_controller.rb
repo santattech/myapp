a7c0c9b2b970faa32f4347e20ee48cb7378ec6d5
Santanu <santattech@gmail.com>
2014-12-24 22:10:40 +0530

    slide initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :site_under_construction
+  before_filter :site_under_construction, only: []
 
   def show
     @user = User.find(params[:id])
